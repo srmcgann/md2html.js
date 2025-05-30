9c7c69203706cca7750ee060401467bd06a6f881
Scott R McGann <s.r.mcgann@hotmail.com>
2025-05-30 13:08:16 -0700

    Add files via upload

---
+++ md2html.js
@@ -0,0 +1,11 @@
+// 'md2html.js', a Markdown converter
+// Scott McGann - whitehotrobot@gmail.com
+// all rights reserved - ©2025
+
+const MarkdownToHTML = (src, el) => {
+  el.innerHTML = src
+}
+
+export {
+  MarkdownToHTML
+}
